ea14d3f260af20af46db4e06f45cfd694e20a36c
Robb <devs1ko.dev@gmail.com>
2025-10-03 10:10:07 +0200

    fix: correct volume name in docker-compose and update database path in users.get.ts

--- server/api/users.get.ts
+++ server/api/users.get.ts
@@ -1,7 +1,7 @@
 import Database from "better-sqlite3";
 
 export default defineEventHandler(async () => {
-  const db = new Database("./data/tarkovpedia.db");
+  const db = new Database("./database/tarkovpedia.db");
   const users = db.prepare("SELECT * FROM user").all();
   db.close();
   return users;
